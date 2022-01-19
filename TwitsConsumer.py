293018e8fcb65d9b535e07646b4b5b20081ac077
nathan <nathanli3636@gmail.com>
2022-01-18 23:24:16 -0800

    Update: remove parameter

--- TwitsConsumer.py
+++ TwitsConsumer.py
@@ -1,12 +1,15 @@
 import json
 from kafka import KafkaConsumer
-
+import configparser
 
 if __name__ == '__main__':
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    server_ip = config['KAFKA']['server_ip']
     topic = 'StockTwits'
     consumer = KafkaConsumer(
         topic,
-        bootstrap_servers='ec2-18-237-169-51.us-west-2.compute.amazonaws.com:9092',
+        bootstrap_servers=server_ip,
         auto_offset_reset='earliest'
     )
     for message in consumer:
